731850b0dbe4589e0d6c6a3e19169c85aa25de20
Carlos-gb86 <carlos.gil@chalmers.se>
2024-02-13 21:32:41 +0100

    first commit

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,24 @@
+export default function FinishScreen({
+  points,
+  maxPoints,
+  highscore,
+  dispatch,
+}) {
+  const percentage = (points / maxPoints) * 100;
+  let emoji = "🥇";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPoints} ({Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
